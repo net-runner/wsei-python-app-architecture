d636b9e2e0bfa2a8d352e7cb44e614b28680426e
net-runner <slazynski.j@gmail.com>
2025-02-19 22:37:09 +0100

    2.2 Iterator Power Generator

---
+++ iterator.py
@@ -0,0 +1,33 @@
+class PowerGenerator:
+    def __init__(self, a, n):
+        self.a = a
+        self.n = n
+        self.current = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.current > self.n:
+            raise StopIteration
+        result = self.a ** self.current
+        self.current += 1
+        return result
+
+gen1 = PowerGenerator(2, 5)
+print(list(gen1))
+
+gen2 = PowerGenerator(3, 4)
+print(list(gen2))
+
+gen3 = PowerGenerator(5, 3)
+print(list(gen3))
+
+def test_power_generator():
+    assert list(PowerGenerator(2, 3)) == [1, 2, 4, 8]
+    assert list(PowerGenerator(3, 2)) == [1, 3, 9]
+    assert list(PowerGenerator(5, 0)) == [1]
+    assert list(PowerGenerator(10, 1)) == [1, 10]
+    print("All tests passed.")
+
+test_power_generator()
